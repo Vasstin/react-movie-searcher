34fbe226da56c0ff0c0370c78eb3507319921fe3
Vasstin <vastin115408@gmail.com>
2021-09-06 14:02:14 +0300

    fixed composeEnhancers for redux devtools

--- src/index.js
+++ src/index.js
@@ -19,10 +19,7 @@ const rootReducer = combineReducers({
   // search: searchReducer
 })
 
-const composeEnhancers =
-  process.env.NODE_ENV === "development"
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
 
 const sagaMiddleware = createSagaMiddleware();
   
